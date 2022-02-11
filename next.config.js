f8a8369b3d1fcf32e2ae7cc123b251e8d0d88cb8
mikecabana <mikeyc343@gmail.com>
2022-02-11 16:36:40 -0500

    bumps versions to latest

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-	images: {
-		domains: ['tailwindui.com'],
-	},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
 };
+
+module.exports = nextConfig;
